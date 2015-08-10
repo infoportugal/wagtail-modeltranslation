63162776f014f1bc6341c7f72f283d7fc20ead70
Rui Manuel da Silva Martins <rmartins16@gmail.com>
2015-08-10 14:46:06 +0100

    Bug #16: Template tags missing from pip install;

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='wagtail-modeltranslation',
-    version='0.2.0',
+    version='0.2.1',
     description='Translates Waghtail CMS models using a registration approach.',
     long_description=(
         'The modeltranslation application can be used to translate dynamic '
