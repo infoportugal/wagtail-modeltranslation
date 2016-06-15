0685e9da9f25260fd6707f2fef3363c2d7b15d99
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2016-06-15 11:50:33 +0100

    Preparing version v0.5.0 alpha 1.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='wagtail-modeltranslation',
-    version='0.5.0a',
+    version='0.5.0a1',
     description='Translates Wagtail CMS models using a registration approach.',
     long_description=(
         'The modeltranslation application can be used to translate dynamic '
