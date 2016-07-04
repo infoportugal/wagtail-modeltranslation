2d54c7751b877b13aefa49ec9991d2d38e587863
Dennis Pratter <d.pratter@nousguide.com>
2016-07-04 16:30:45 +0200

    Use POST requests to copy StreamFields, Fixes #74

--- wagtail_modeltranslation/static/modeltranslation/js/copy_stream_fields.js
+++ wagtail_modeltranslation/static/modeltranslation/js/copy_stream_fields.js
@@ -49,7 +49,7 @@ function requestCopyField(originID, targetID) {
 	 */
 	$.ajax({
 		url: 'copy_translation_content',
-		type: 'GET',
+		type: 'POST',
 		dataType: 'json',
 		data: {'origin_field_name': originID, 'target_field_name': targetID, 'serializedOriginField': jsonString},
 	})
