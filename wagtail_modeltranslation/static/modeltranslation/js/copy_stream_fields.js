ceac4a679c0b178725ee60b76510640e6169ab9f
pyMan <claudiomarinozzi78@gmail.com>
2016-12-21 10:02:19 +0100

    Fix StreamField inside InlinePanel
    If a StreamField is used inside an InlinePanel, there will be no 'h2'
    header anymore and we get js errors and things stop working.
    This modification checks for the existance of the 'h2' tag and if not
    found it will ignore this streamfield for creating useless (in this
    case) language copy buttons.

--- wagtail_modeltranslation/static/modeltranslation/js/copy_stream_fields.js
+++ wagtail_modeltranslation/static/modeltranslation/js/copy_stream_fields.js
@@ -9,6 +9,8 @@ $(document).ready(function(){
 		var currentStreamField = allStreamFields[i];
 		//Current Field header
 		var header = $(currentStreamField).children('h2')[0];
+		if (!header) //True if StreamField inside InlinePanel
+			continue;
 		//Search for the input field so that we can get is id to know the field's name.
 		var streamFieldDiv = $(currentStreamField).find('div.sequence-container.sequence-type-stream')[0];
 		var fieldId = $(streamFieldDiv).children('input')[0].id;
