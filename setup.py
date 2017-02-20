f8a49c493b48178daf20ac67a923b00b978c41c8
Alexandre Silva <amsilva@infoportugal.impresa.pt>
2017-02-20 14:24:29 +0000

    Changed original setup.py to add django-modeltranslation as a dependency. Automatic installation of project dependencies (wagtail and django-modeltranslation).

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='wagtail-modeltranslation',
+    version='0.6rc1',
+    description='Translates Wagtail CMS models using a registration approach.',
+    long_description=(
+        'The modeltranslation application can be used to translate dynamic '
+        'content of existing models to an arbitrary number of languages '
+        'without having to change the original model classes. It uses a '
+        'registration approach (comparable to Django\'s admin app) to be able '
+        'to add translations to existing or new projects and is fully '
+        'integrated into the Wagtail admin backend.'),
+    author='InfoPortugal, S.A.',
+    author_email='suporte24@infoportugal.pt',
+    maintainer='InfoPortugal, S.A.',
+    maintainer_email='suporte24@infoportugal.pt',
+    url='https://github.com/infoportugal/wagtail-modeltranslation',
+    packages=[],
+    install_requires=['wagtail(>=1.9)', 'django-modeltranslation(<=0.12.99)'],
+    download_url='https://github.com/infoportugal/wagtail-modeltranslation/archive/v0.6rc1.tar.gz',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Operating System :: OS Independent',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License'],
+    license='New BSD')
