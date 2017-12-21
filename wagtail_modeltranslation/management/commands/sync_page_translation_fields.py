333c392dc8139e0a2cd3256daed56edc9c064142
Dario Marcelino <dario@appscot.com>
2017-12-21 18:58:07 +0000

    Move Page translation fields to Page table (breaking changes!)
    - Bumps version to 0.8.0-alpha
    - Introduces 'makemigrations_translation' command to avoid generating
    wagtailcore_page migrations
    - Adds 'sync_page_translation_fields' which mimicks
    'sync_translation_fields' but restricts it to Page model
    - This change introduces breaking changes and has no migration script to
    move from 0.6 to 0.8
    - Updates tests

---
+++ wagtail_modeltranslation/management/commands/sync_page_translation_fields.py
@@ -0,0 +1,26 @@
+from modeltranslation.management.commands.sync_translation_fields import Command as SyncTranslationsFieldsCommand
+from modeltranslation.translator import translator
+from wagtail.wagtailcore.models import Page
+
+
+old_get_registered_models = translator.get_registered_models
+
+# Monkey patching, only return a model if it's Page
+def get_page_model(self, abstract=True):
+    models = old_get_registered_models(abstract)
+    return [x for x in models if x is Page]
+
+
+class Command(SyncTranslationsFieldsCommand):
+    help = ("Detect new translatable fields or new available languages and"
+            " sync Wagtail's Page database structure. Does not remove "
+            " columns of removed languages or undeclared fields.")
+
+    def handle(self, *args, **options):
+        translator.get_registered_models = get_page_model.__get__(translator)
+
+        try:
+            super(Command, self).handle(*args, **options)
+
+        finally:
+            translator.get_registered_models = old_get_registered_models
