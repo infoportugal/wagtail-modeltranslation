8000594114fdc6936fc9279a8269d2c7b7eb08eb
Dario Marcelino <dario@appscot.com>
2018-01-03 17:52:21 +0000

    Add --dependency-links flag to pip install

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ setup(
         'wagtail_modeltranslation.templatetags'],
     package_data={'wagtail_modeltranslation': ['static/wagtail_modeltranslation/css/*.css',
                                                'static/wagtail_modeltranslation/js/*.js']},
-    install_requires=['wagtail(>=1.4)', 'django-modeltranslation(<=0.12.99)'],
+    install_requires=['wagtail(>=1.4)', 'django-modeltranslation(>0.12.1)'],
     dependency_links=[
-        "git+ssh://git@github.com/deschler/django-modeltranslation.git@00fc7f1804aaa1b1e37af48e67871080851e14b0#egg=django-modeltranslation-0.12.1"
+        "http://github.com/deschler/django-modeltranslation/tarball/00fc7f1804aaa1b1e37af48e67871080851e14b0#egg=django-modeltranslation-0.12.2"
     ],
     download_url='https://github.com/infoportugal/wagtail-modeltranslation/archive/v0.6rc2.tar.gz',
     classifiers=[
