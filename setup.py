0b81be9d6e77e4e6af346798d650fee5eed0a63a
DiogoMarques <doriva.marques.29@gmail.com>
2018-01-15 14:26:03 +0000

    Final version for setup.py 0.8 alpha

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='wagtail-modeltranslation',
-    version='0.8.0-alpha',
+    version='0.8a1',
     description='Translates Wagtail CMS models using a registration approach.',
     long_description=(
         'The modeltranslation application can be used to translate dynamic '
