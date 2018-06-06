e822595e8e81e552ce84093ca8379dbaeaf4ba0d
Pomax <pomax@nihongoresources.com>
2018-06-06 16:01:20 -0700

    improved fieldset abstraction and safer toggler class

--- wagtail_modeltranslation/static/wagtail_modeltranslation/js/language_toggles.js
+++ wagtail_modeltranslation/static/wagtail_modeltranslation/js/language_toggles.js
@@ -18,36 +18,58 @@ if (topLevel.attr(`class`) && topLevel.attr(`class`).indexOf(`search`) > -1) {
 }
 
 /**
+ * ...
+ */
+function filterForLocale(index, element) {
+  var tc = element.textContent;
+  var res = tc.match(/ \[\w\w\]/);
+  if (res === null) return;
+
+  var code = res[0],
+      locale = code.replace(/[ \[\]]/g,'');
+
+  // Verify this is a known locale and not a fluke,
+  // using the global "langs" variable, which is an
+  // array of all language codes specified in the
+  // settings.LANGUAGES variable for Django.
+  if (langs.indexOf(locale) === -1) return;
+
+  // We do our show/hiding based on list items,
+  // otherwise we're just "emptying" a list item
+  // while leaving its spacing CSS intact.
+  if (element.nodeName !== "LI") {
+    element = $(element).closest("li")[0];
+  }
+
+  // Bootstrap an empty bin if we don't have one.
+  if (!localisedElements[locale]) {
+    localisedElements[locale] = [];
+  }
+
+  // Add this element to our bin, provided it had
+  // not already been added.
+  var bin = localisedElements[locale];
+  if (bin.indexOf(element) === -1) {
+    bin.push(element);
+    element.classList.add(`l10n-hidden`);
+
+    // also note that "field-col" elements may now look horribly
+    // wrong, due to how Wagtail computes which of "col3"..."col12"
+    // to use. Because wagtail-modeltranslation introduces many more
+    // elements to show in an "inline" element, things that were
+    // "col6" before end up being "col1", looking terribly wrong indeed.
+    element.classList.remove(...columnCSS);
+  }
+}
+
+/**
  * Build the set of fields-per-locale. Each set will receive
  * a button to toggle visibility for all fields in that set,
  * with the note that unlocalised content (such as images)
  * will always stay visible.
  */
 function buildSets() {
-  // Note: we do NOT rely on the "langs" variable
-  // that wagtail-modeltranslations sets up, as
-  // we want to know whether "this page",
-  // specifically, has any localised fields that
-  // require set building, rather than which URL
-  // language prefixes are supported, in general.
-  var localisedElements = {};
-
-  $(`li.object`, topLevel).each( (index, element) => {
-    var tc = element.textContent;
-    var res = tc.match(/\[\w\w\]/);
-    if (res === null) return;
-
-    var locale = res[0].replace(/[\[\]]/g,'');
-
-    if (!localisedElements[locale]) {
-      localisedElements[locale] = [];
-    }
-
-    localisedElements[locale].push(element);
-    element.classList.add(`l10n-hidden`);
-  });
-
-  return localisedElements;
+  $(`li.object, div.field`, topLevel).each( filterForLocale );
 }
 
 /**
@@ -67,9 +89,9 @@ function buildLocaleToggler() {
     $(`button.locale-toggle`, li).each( (index, toggle) => {
       toggle.addEventListener(`click`, e => {
         e.preventDefault();
-        toggle.classList.toggle(`active`);
+        toggle.classList.toggle(`showing-locale`);
         toggleLocale(locale);
-      })
+      });
 
       toggles[locale] = toggle;
     });
@@ -99,7 +121,14 @@ function toggleLocale(locale, state) {
 }
 
 var default_locale = `en`;
-var localisedElements = buildSets();
+var localisedElements = {};
+var columnCSS = [`field-col`];
+for (var i=1; i<=12; i++) { columnCSS.push(`col${i}`); }
+
+// Build the sets that track which fields
+// belong to which language code.
+buildSets();
+
 var locales = Object.keys(localisedElements).sort();
 
 // If there are no locale sets, then there is
