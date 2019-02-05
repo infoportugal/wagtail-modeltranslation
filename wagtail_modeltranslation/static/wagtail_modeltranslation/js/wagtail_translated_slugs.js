1db9f3947ccaf1ee0ac9ab099f0840050f550262
Diogo Marques <diogomarques@Diogos-MacBook-Pro.local>
2019-02-05 10:59:51 +0000

    Auto populate slug when not translated

--- wagtail_modeltranslation/static/wagtail_modeltranslation/js/wagtail_translated_slugs.js
+++ wagtail_modeltranslation/static/wagtail_modeltranslation/js/wagtail_translated_slugs.js
@@ -1,23 +1,36 @@
 $(document).ready(function () {
     /* Only non-live pages should auto-populate the slug from the title */
     if (!$('body').hasClass('page-is-live')) {
-        var slugFollowsTitle = false;
-
-        $.each(langs, function (idx, lang_code) {
-            lang_code = lang_code.replace("-", "_");
-            $('#id_title_' + lang_code).on('focus', function () {
-                /* slug should only follow the title field if its value matched the title's value at the time of focus */
-                var currentSlug = $('#id_slug_' + lang_code).val();
-                var slugifiedTitle = cleanForSlug(this.value, true);
-                slugFollowsTitle = (currentSlug == slugifiedTitle);
+        if(!translate_slugs) {
+            lang_code = default_lang.replace("-", "_");
+            title_selector = '#id_title_' + lang_code;
+            slug_selector = '#id_slug';
+            slugAutoPopulateTranslation(title_selector, slug_selector);
+        } else {
+            $.each(langs, function (idx, lang_code) {
+                lang_code = lang_code.replace("-", "_");
+                title_selector = '#id_title_' + lang_code;
+                slug_selector = '#id_slug_' + lang_code;
+                slugAutoPopulateTranslation(title_selector, slug_selector);
             });
-
-            $('#id_title_' + lang_code).on('keyup keydown keypress blur', function () {
-                if (slugFollowsTitle) {
-                    var slugifiedTitle = cleanForSlug(this.value, true);
-                    $('#id_slug_' + lang_code).val(slugifiedTitle);
-                }
-            });
-        });
+        }
     }
 });
+
+function slugAutoPopulateTranslation(title_selector, slug_selector) {
+    var slugFollowsTitle = false;
+
+    $(title_selector).on('focus', function () {
+        /* slug should only follow the title field if its value matched the title's value at the time of focus */
+        var currentSlug = $(slug_selector).val();
+        var slugifiedTitle = cleanForSlug(this.value, true);
+        slugFollowsTitle = (currentSlug == slugifiedTitle);
+    });
+
+    $(title_selector).on('keyup keydown keypress blur', function () {
+        if (slugFollowsTitle) {
+            var slugifiedTitle = cleanForSlug(this.value, true);
+            $(slug_selector).val(slugifiedTitle);
+        }
+    });
+}
