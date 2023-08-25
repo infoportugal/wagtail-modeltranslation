dc5439803104a185c8707a3dcd5129fb36390ef1
Diogo Marques <dmarques@infoportugal.impresa.pt>
2023-08-25 16:42:21 +0100

    ADD master_doc index

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,8 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
